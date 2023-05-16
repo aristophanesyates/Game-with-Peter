f611b0e7f597fc8e8fb0f8d829ff29dd60988ee4
good_news <marymag69@outlook.com>
2023-05-16 22:05:17 +0100

    Knife iteration
    - knives stick in objects (logic moved to Projectile)
    - enemies drop stuck knives on Die()
    - knives cannot stick into object with the "Impenetrable" tag
    - added Impenetrable tag to knives and floor.

--- Assets/Scripts/Combat/CharacterValues/Health.cs
+++ Assets/Scripts/Combat/CharacterValues/Health.cs
@@ -19,11 +19,20 @@ public class Health : MonoBehaviour
         {
             Die();
         }
-        Debug.Log(gameObject.name + ": Ow!");
+        Debug.Log($"{gameObject.name}: Ow! Now I only have {health} health!");
         lastStruckBy = source;
     }
     public void Die()
     {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Transform t = transform.GetChild(i);
+            if (t.GetComponent<Projectile>())
+            {
+                t.SetParent(null);
+                t.GetComponent<Rigidbody>().isKinematic = false;
+            }
+        }
         Debug.Log(gameObject.name + ": Blech!");
         Destroy(gameObject);
     }
--- Assets/Scripts/Combat/Weapons/Projectile.cs
+++ Assets/Scripts/Combat/Weapons/Projectile.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.Utilities;
 
 public class Projectile : MonoBehaviour
 {
     public Rigidbody rb;
+    private bool nullified;
+    void Awake()
+    {
+        nullified = false;
+    }
     public static void SpawnProjectile(GameObject prefab, Vector3 position, Quaternion orientation, Vector3 velocity, Vector3 localSpin, float scale = 1)
     {
         GameObject proj = Instantiate<GameObject>(prefab, position, orientation);
@@ -23,15 +29,27 @@ public class Projectile : MonoBehaviour
     }
     private void OnCollisionEnter(Collision collision)
     {
+        if (nullified)
+        { 
+            return;
+        }
         GameObject other = collision.gameObject;
+        if (other.tag == "Impenetrable")
+        {
+            return;
+        }
+        Vector3 strikeVector = rb.velocity.normalized;
         Debug.Log(other.name + " was struck by a knife!");
+        gameObject.transform.rotation *= Quaternion.FromToRotation(transform.forward, strikeVector);
+        gameObject.transform.SetParent(other.transform);
+        rb.isKinematic = true;
         Health healthObject = other.GetComponent<Health>();
         if (healthObject)
         {
             Debug.Log("Calling " + other.name + "'s Struck() function. Base Damage: " + 2);
             healthObject.Struck(2, gameObject);
-
         }
+        nullified = true;
     }
     // Start is called before the first frame update
     void Start()
